7a7e37dc51ea508c38a32780f2c6a15b6fb8d315
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-09-15 18:01:00 -0500

    Updates

---
+++ precache-manifest.766688d9c5548c267a30dfc4615cea58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cedaa4dfade56fad94fcaa4048bdea",
+    "url": "/react-landing-404/index.html"
+  },
+  {
+    "revision": "020177e384f6f82d5520",
+    "url": "/react-landing-404/static/css/main.51bcc3d7.chunk.css"
+  },
+  {
+    "revision": "7025137b437b95f304f8",
+    "url": "/react-landing-404/static/js/2.e429d9f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-landing-404/static/js/2.e429d9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "020177e384f6f82d5520",
+    "url": "/react-landing-404/static/js/main.3dbb922f.chunk.js"
+  },
+  {
+    "revision": "6c207eb2500b319be924",
+    "url": "/react-landing-404/static/js/runtime-main.65fde4d9.js"
+  },
+  {
+    "revision": "ba0a8362db3759ad0db60a2fa8c2405b",
+    "url": "/react-landing-404/static/media/main-astronaut.ba0a8362.png"
+  },
+  {
+    "revision": "e0c879ba46204e1ccede4e377fa9a4f9",
+    "url": "/react-landing-404/static/media/moon.e0c879ba.png"
+  }
+]);
